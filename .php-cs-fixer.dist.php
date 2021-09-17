35823b578bf4952690e5f170e559ad65413ca394
Jon Cram <webignition@gmail.com>
2021-09-17 15:48:58 +0100

    Enforce declaration of strict types (#17)

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__ . '/bin')
     ->in(__DIR__ . '/public')
@@ -17,6 +19,7 @@ return (new PhpCsFixer\Config())
         'trailing_comma_in_multiline' => false,
         'php_unit_internal_class' => false,
         'php_unit_test_class_requires_covers' => false,
+        'declare_strict_types' => true,
     ])
     ->setFinder($finder)
     ;
