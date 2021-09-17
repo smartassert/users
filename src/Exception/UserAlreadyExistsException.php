580e87d118b794ea243068312b00d8373940c7db
Jon Cram <webignition@gmail.com>
2021-09-17 17:44:18 +0100

    Create UserFactory (#20)

---
+++ src/Exception/UserAlreadyExistsException.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exception;
+
+use App\Entity\User;
+
+class UserAlreadyExistsException extends \Exception
+{
+    public function __construct(
+        private User $user
+    ) {
+        parent::__construct(sprintf(
+            'User "%s" already exists',
+            $user->getEmail()
+        ));
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+}
