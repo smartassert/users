33e057c37da0ef0639b97ead738452e00bea897d
Jon Cram <webignition@gmail.com>
2021-09-17 18:25:26 +0100

    Move routing definition to controller annotations

--- src/Controller/TokenController.php
+++ src/Controller/TokenController.php
@@ -5,8 +5,14 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class TokenController extends AbstractController
 {
     public const ROUTE_CREATE = '/token/create';
+
+    #[Route(self::ROUTE_CREATE, name: 'token_create')]
+    public function create(): void
+    {
+    }
 }
