46cc810bd9c39cf121607dfd53acf548880fe6f9
Jon Cram <webignition@gmail.com>
2021-09-20 16:43:39 +0100

    Add User::jsonSerialize()

---
+++ tests/Unit/Entity/UserTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Entity;
+
+use App\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * @dataProvider jsonSerializeDataProvider
+     *
+     * @param array<mixed> $expectedSerializedUser
+     */
+    public function testJsonSerialize(User $user, array $expectedSerializedUser): void
+    {
+        self::assertSame($expectedSerializedUser, $user->jsonSerialize());
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function jsonSerializeDataProvider(): array
+    {
+        return [
+            'default' => [
+                'user' => new User(
+                    'id-value',
+                    'email-value',
+                    'password-value'
+                ),
+                'expectedSerializedUser' => [
+                    'id' => 'id-value',
+                    'email' => 'email-value',
+                ],
+            ],
+        ];
+    }
+}
