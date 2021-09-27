cca966e11ce78c93824c102b45eaf6a26f155762
Jon Cram <webignition@gmail.com>
2021-09-27 16:25:38 +0100

    Create ApiKey entity (#58)

---
+++ tests/Functional/Entity/ApiKeyTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Entity;
+
+use App\Entity\ApiKey;
+use App\Entity\User;
+use App\Repository\ApiKeyRepository;
+use App\Tests\Services\UserRemover;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ApiKeyTest extends WebTestCase
+{
+    private EntityManagerInterface $entityManager;
+    private ApiKeyRepository $apiKeyRepository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $entityManager = self::getContainer()->get(EntityManagerInterface::class);
+        \assert($entityManager instanceof EntityManagerInterface);
+        $this->entityManager = $entityManager;
+
+        $apiKeyRepository = self::getContainer()->get(ApiKeyRepository::class);
+        \assert($apiKeyRepository instanceof ApiKeyRepository);
+        $this->apiKeyRepository = $apiKeyRepository;
+
+        $this->removeAllUsers();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->removeAllUsers();
+
+        parent::tearDown();
+    }
+
+    public function testEntityMapping(): void
+    {
+        $user = new User(
+            '01234567890123456789012345678901',
+            'user@example.com',
+            'hashed-password'
+        );
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        self::assertCount(0, $this->apiKeyRepository->findAll());
+
+        $apiKey = new ApiKey(
+            '98765432109876543210987654321098',
+            'label value',
+            $user
+        );
+
+        $this->entityManager->persist($apiKey);
+        $this->entityManager->flush();
+    }
+
+    private function removeAllUsers(): void
+    {
+        $userRemover = self::getContainer()->get(UserRemover::class);
+        if ($userRemover instanceof UserRemover) {
+            $userRemover->removeAll();
+        }
+    }
+}
