c70b41bed16e55349652bba4b8bf6c7d4230523e
Jon Cram <webignition@gmail.com>
2021-09-27 16:25:38 +0100

    Create ApiKey entity (#58)

---
+++ migrations/Version20210927145717.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20210927145717 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('
+            CREATE TABLE api_key (
+                id VARCHAR(32) NOT NULL, 
+                owner_id VARCHAR(32) NOT NULL, 
+                label VARCHAR(255) NOT NULL, 
+                PRIMARY KEY(id)
+            )
+        ');
+        $this->addSql('CREATE INDEX IDX_C912ED9D7E3C61F9 ON api_key (owner_id)');
+        $this->addSql('
+            ALTER TABLE api_key 
+                ADD CONSTRAINT FK_C912ED9D7E3C61F9 
+                FOREIGN KEY (owner_id) REFERENCES "user" (id) 
+                NOT DEFERRABLE INITIALLY IMMEDIATE
+        ');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE api_key');
+    }
+}
