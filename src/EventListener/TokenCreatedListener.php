580537ab7f51e771e5f0d35627149d1bfb76662b
Jon Cram <webignition@gmail.com>
2021-09-28 09:15:39 +0100

    Remove UserRepository from TokenCreatedListener (#67)

--- src/EventListener/TokenCreatedListener.php
+++ src/EventListener/TokenCreatedListener.php
@@ -4,24 +4,17 @@ declare(strict_types=1);
 
 namespace App\EventListener;
 
-use App\Entity\User;
-use App\Repository\UserRepository;
+use App\Security\IdentifiableUserInterface;
 use App\Security\TokenInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
 
 class TokenCreatedListener
 {
-    public function __construct(
-        private UserRepository $userRepository,
-    ) {
-    }
-
     public function __invoke(JWTCreatedEvent $event): void
     {
         $payload = $event->getData();
-
-        $user = $this->userRepository->findByEmail($payload[TokenInterface::CLAIM_USER_IDENTIFIER] ?? '');
-        if ($user instanceof User) {
+        $user = $event->getUser();
+        if ($user instanceof IdentifiableUserInterface) {
             $payload[TokenInterface::CLAIM_USER_ID] = $user->getId();
         }
 
