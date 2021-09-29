7e3986e889dceb6188f48cf1e3ca15205988cf91
Jon Cram <webignition@gmail.com>
2021-09-29 15:08:22 +0100

    Re-add frontend create token tests

--- tests/Functional/Token/Frontend/CreateTest.php
+++ tests/Functional/Token/Frontend/CreateTest.php
@@ -7,21 +7,55 @@ namespace App\Tests\Functional\Token\Frontend;
 use App\Security\AudienceClaimInterface;
 use App\Security\TokenInterface;
 use App\Tests\Functional\AbstractBaseWebTestCase;
+use App\Tests\Services\Asserter\ResponseAsserter\ArrayBodyAsserter;
 use App\Tests\Services\Asserter\ResponseAsserter\JsonResponseAsserter;
 use App\Tests\Services\Asserter\ResponseAsserter\JwtTokenBodyAsserterFactory;
 use App\Tests\Services\TestUserFactory;
+use Doctrine\ORM\EntityManagerInterface;
+use Gesdinet\JWTRefreshTokenBundle\Entity\RefreshToken;
+use Gesdinet\JWTRefreshTokenBundle\Entity\RefreshTokenRepository;
 
 class CreateTest extends AbstractBaseWebTestCase
 {
+    private EntityManagerInterface $entityManager;
+    private RefreshTokenRepository $refreshTokenRepository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $entityManager = self::getContainer()->get(EntityManagerInterface::class);
+        \assert($entityManager instanceof EntityManagerInterface);
+        $this->entityManager = $entityManager;
+
+        $refreshTokenRepository = $entityManager->getRepository(RefreshToken::class);
+        \assert($refreshTokenRepository instanceof RefreshTokenRepository);
+        $this->refreshTokenRepository = $refreshTokenRepository;
+
+        $this->removeAllRefreshTokens();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->removeAllRefreshTokens();
+    }
+
     public function testCreateSuccess(): void
     {
         $testUserFactory = self::getContainer()->get(TestUserFactory::class);
         \assert($testUserFactory instanceof TestUserFactory);
 
-        $this->removeAllUsers();
+        self::assertCount(0, $this->refreshTokenRepository->findAll());
 
         $user = $testUserFactory->create();
         $response = $this->application->makeFrontendCreateTokenRequest(...$testUserFactory->getCredentials());
+
+        $refreshTokens = $this->refreshTokenRepository->findAll();
+        self::assertCount(1, $refreshTokens);
+        $refreshToken = $refreshTokens[0];
+        self::assertInstanceOf(RefreshToken::class, $refreshToken);
 
         $jwtTokenBodyAsserterFactory = self::getContainer()->get(JwtTokenBodyAsserterFactory::class);
         \assert($jwtTokenBodyAsserterFactory instanceof JwtTokenBodyAsserterFactory);
@@ -37,6 +71,11 @@ class CreateTest extends AbstractBaseWebTestCase
                     ],
                 ]
             ))
+            ->addBodyAsserter(
+                new ArrayBodyAsserter([
+                    'refresh_token' => $refreshToken->getRefreshToken(),
+                ])
+            )
             ->assert($response)
         ;
     }
@@ -46,5 +85,15 @@ class CreateTest extends AbstractBaseWebTestCase
         $response = $this->application->makeFrontendCreateTokenRequest('', '');
 
         self::assertSame(401, $response->getStatusCode());
+    }
+
+    private function removeAllRefreshTokens(): void
+    {
+        $refreshTokens = $this->refreshTokenRepository->findAll();
+
+        foreach ($refreshTokens as $refreshToken) {
+            $this->entityManager->remove($refreshToken);
+            $this->entityManager->flush();
+        }
     }
 }
