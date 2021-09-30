49512a6ae167a65d97d231974896702cfd92b69e
Jon Cram <webignition@gmail.com>
2021-09-30 10:38:49 +0100

    Log in via /frontend/token/create

--- tests/Functional/RefreshToken/CustomRefreshTest.php
+++ tests/Functional/RefreshToken/CustomRefreshTest.php
@@ -4,27 +4,59 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional\RefreshToken;
 
+use App\Tests\Services\TestUserFactory;
+use App\Tests\Services\UserRemover;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 final class CustomRefreshTest extends WebTestCase
 {
+    private KernelBrowser $client;
+
+    /**
+     * @var array{"userIdentifier": string, "password": string}
+     */
+    private array $testUserCredentials;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->client = self::createClient();
+
+        $userRemover = self::getContainer()->get(UserRemover::class);
+        if ($userRemover instanceof UserRemover) {
+            $userRemover->removeAll();
+        }
+
+        $testUserFactory = self::getContainer()->get(TestUserFactory::class);
+        if ($testUserFactory instanceof TestUserFactory) {
+            $testUserFactory->create();
+            $this->testUserCredentials = $testUserFactory->getCredentials();
+        }
+    }
+
     public function testLoginAndRefreshApiToken(): void
     {
-        $client = self::createClient();
-
-        $client->request(
+        $this->client->request(
             method: 'POST',
-            uri: '/api/login',
+            uri: '/frontend/token/create',
             server: [
                 'CONTENT_TYPE' => 'application/json',
                 'HTTP_ACCEPT' => 'application/json',
             ],
-            content: json_encode(['username' => 'user1', 'password' => 'password'], \JSON_THROW_ON_ERROR)
+            content: json_encode(
+                [
+                    'username' => $this->testUserCredentials['userIdentifier'],
+                    'password' => $this->testUserCredentials['password']
+                ],
+                \JSON_THROW_ON_ERROR
+            )
         );
 
         $this->assertResponseIsSuccessful('Could not authenticate to the API.');
 
-        $response = $client->getResponse();
+        $response = $this->client->getResponse();
 
         if (false === $response->getContent()) {
             $this->fail('The API did not send a proper response.');
@@ -35,7 +67,7 @@ final class CustomRefreshTest extends WebTestCase
         $this->assertArrayHasKey('token', $data, 'JWT token is not in the response');
         $this->assertArrayHasKey('refresh_token', $data, 'Refresh token is not in the response');
 
-        $client->request(
+        $this->client->request(
             method: 'POST',
             uri: '/api/token/refresh',
             server: [
@@ -47,7 +79,7 @@ final class CustomRefreshTest extends WebTestCase
 
         $this->assertResponseIsSuccessful('Could not refresh the API token.');
 
-        $response = $client->getResponse();
+        $response = $this->client->getResponse();
 
         if (false === $response->getContent()) {
             $this->fail('The API did not send a proper response.');
