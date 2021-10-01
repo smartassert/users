1eb251fe048863ae802d661be6a01106ccb56d22
Jon Cram <webignition@gmail.com>
2021-10-01 12:05:31 +0100

    Return empty 401 response for admin firewall authentication failures (#114)

---
+++ src/Security/Admin/AuthenticationEntryPoint.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security\Admin;
+
+use App\Response\UnauthorizedResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+
+class AuthenticationEntryPoint implements AuthenticationEntryPointInterface
+{
+    public function start(Request $request, AuthenticationException $authException = null): Response
+    {
+        return new UnauthorizedResponse();
+    }
+}
