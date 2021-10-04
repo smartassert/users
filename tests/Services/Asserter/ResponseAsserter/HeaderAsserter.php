aa9a301521d5ba7b4051207dd0eb306cfa3a9e35
Jon Cram <webignition@gmail.com>
2021-10-04 09:36:18 +0100

    HeaderAsserter ignores key case

--- tests/Services/Asserter/ResponseAsserter/HeaderAsserter.php
+++ tests/Services/Asserter/ResponseAsserter/HeaderAsserter.php
@@ -9,15 +9,28 @@ use PHPUnit\Framework\Assert;
 class HeaderAsserter implements HeaderAsserterInterface
 {
     /**
+     * @var array<string, string>
+     */
+    private array $expectedHeaders = [];
+
+    /**
      * @param array<string, string> $expectedHeaders
      */
     public function __construct(
-        private array $expectedHeaders = [],
+        array $expectedHeaders = [],
     ) {
+        foreach ($expectedHeaders as $key => $value) {
+            $this->expectedHeaders[strtolower($key)] = $value;
+        }
     }
 
     public function assert(array $headers): void
     {
+        foreach ($headers as $key => $value) {
+            unset($headers[$key]);
+            $headers[strtolower($key)] = $value;
+        }
+
         foreach ($this->expectedHeaders as $expectedKey => $expectedValue) {
             Assert::assertArrayHasKey($expectedKey, $headers);
             Assert::assertSame($expectedValue, $headers[$expectedKey][0]);
