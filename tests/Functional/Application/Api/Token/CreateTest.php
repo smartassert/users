ca88a44f760a7ac4cd868111e9487b2d3ee0b94c
Jon Cram <webignition@gmail.com>
2022-08-17 18:25:12 +0100

    Create /api/token/create test (#311)

--- tests/Functional/Application/Api/Token/CreateTest.php
+++ tests/Functional/Application/Api/Token/CreateTest.php
@@ -2,11 +2,11 @@
 
 declare(strict_types=1);
 
-namespace App\Tests\Integration\Api\Token;
+namespace App\Tests\Functional\Application\Api\Token;
 
 use App\Tests\Application\Api\Token\AbstractCreateTest;
-use App\Tests\Integration\Admin\GetAdminTokenTrait;
-use App\Tests\Integration\GetClientAdapterTrait;
+use App\Tests\Functional\Application\Admin\GetAdminTokenTrait;
+use App\Tests\Functional\Application\GetClientAdapterTrait;
 
 class CreateTest extends AbstractCreateTest
 {
