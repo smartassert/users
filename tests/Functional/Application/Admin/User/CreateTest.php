af65980a204596e18b873647e82b517184be8004
Jon Cram <webignition@gmail.com>
2022-08-17 16:14:24 +0100

    Create /admin/user/create test (#310)

--- tests/Functional/Application/Admin/User/CreateTest.php
+++ tests/Functional/Application/Admin/User/CreateTest.php
@@ -2,13 +2,13 @@
 
 declare(strict_types=1);
 
-namespace App\Tests\Functional\Application\Admin\Frontend\RefreshToken;
+namespace App\Tests\Functional\Application\Admin\User;
 
-use App\Tests\Application\Admin\Frontend\RefreshToken\AbstractRevokeTest;
+use App\Tests\Application\Admin\User\AbstractCreateTest;
 use App\Tests\Functional\Application\Admin\GetAdminTokenTrait;
 use App\Tests\Functional\Application\GetClientAdapterTrait;
 
-class RevokeTest extends AbstractRevokeTest
+class CreateTest extends AbstractCreateTest
 {
     use GetClientAdapterTrait;
     use GetAdminTokenTrait;
--- tests/Integration/Admin/Frontend/RefreshToken/RevokeTest.php
+++ tests/Integration/Admin/Frontend/RefreshToken/RevokeTest.php
@@ -5,17 +5,11 @@ declare(strict_types=1);
 namespace App\Tests\Integration\Admin\Frontend\RefreshToken;
 
 use App\Tests\Application\Admin\Frontend\RefreshToken\AbstractRevokeTest;
+use App\Tests\Integration\Admin\GetAdminTokenTrait;
 use App\Tests\Integration\GetClientAdapterTrait;
 
 class RevokeTest extends AbstractRevokeTest
 {
     use GetClientAdapterTrait;
-
-    protected function getAdminToken(): string
-    {
-        $adminToken = self::getContainer()->getParameter('primary-admin-token');
-        \assert(is_string($adminToken));
-
-        return $adminToken;
-    }
+    use GetAdminTokenTrait;
 }
