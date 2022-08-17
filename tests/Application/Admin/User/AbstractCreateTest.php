d8ee7d26232ccc560c403ad47ccec314e1303974
Jon Cram <webignition@gmail.com>
2022-08-17 16:06:26 +0100

    Create /admin/user/create test

---
+++ tests/Application/Admin/User/AbstractCreateTest.php
@@ -0,0 +1,190 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Application\Admin\User;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Tests\Application\AbstractApplicationTest;
+
+abstract class AbstractCreateTest extends AbstractApplicationTest
+{
+    private UserRepository $userRepository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $userRepository = self::getContainer()->get(UserRepository::class);
+        \assert($userRepository instanceof UserRepository);
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * @dataProvider createBadMethodDataProvider
+     */
+    public function testCreateBadMethod(string $method): void
+    {
+        $response = $this->applicationClient->makeAdminCreateUserRequest(
+            'email',
+            'password',
+            $this->getAdminToken(),
+            $method
+        );
+
+        self::assertSame(405, $response->getStatusCode());
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function createBadMethodDataProvider(): array
+    {
+        return [
+            'GET' => [
+                'method' => 'GET',
+            ],
+            'PUT' => [
+                'method' => 'PUT',
+            ],
+            'DELETE' => [
+                'method' => 'DELETE',
+            ],
+        ];
+    }
+
+    public function testCreateUnauthorized(): void
+    {
+        $response = $this->applicationClient->makeAdminCreateUserRequest(
+            'email',
+            'password',
+            'invalid admin token'
+        );
+
+        self::assertSame(401, $response->getStatusCode());
+        self::assertSame('', $response->getBody()->getContents());
+    }
+
+    /**
+     * @dataProvider createBadRequestValueMissingDataProvider
+     */
+    public function testCreateBadRequestValueMissing(
+        ?string $email,
+        ?string $password,
+        string $expectedMissingField
+    ): void {
+        $response = $this->applicationClient->makeAdminCreateUserRequest(
+            $email,
+            $password,
+            $this->getAdminToken()
+        );
+
+        self::assertSame(400, $response->getStatusCode());
+        self::assertSame('application/json', $response->getHeaderLine('content-type'));
+        self::assertSame(
+            [
+                'message' => 'Value for field "' . $expectedMissingField . '" missing',
+            ],
+            json_decode($response->getBody()->getContents(), true)
+        );
+
+        self::assertSame(0, $this->userRepository->count([]));
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function createBadRequestValueMissingDataProvider(): array
+    {
+        return [
+            'email missing' => [
+                'email' => null,
+                'password' => 'non-empty value',
+                'expectedMissingField' => 'email',
+            ],
+            'password missing' => [
+                'email' => 'non-empty value',
+                'password' => null,
+                'expectedMissingField' => 'password',
+            ],
+        ];
+    }
+
+    public function testCreateBadRequestUserAlreadyExists(): void
+    {
+        $email = 'user@example.com';
+        $password = 'password';
+
+        $successResponse = $this->applicationClient->makeAdminCreateUserRequest(
+            $email,
+            $password,
+            $this->getAdminToken()
+        );
+
+        self::assertSame(200, $successResponse->getStatusCode());
+        self::assertSame(1, $this->userRepository->count([]));
+
+        $badRequestResponse = $this->applicationClient->makeAdminCreateUserRequest(
+            $email,
+            $password,
+            $this->getAdminToken()
+        );
+
+        self::assertSame(1, $this->userRepository->count([]));
+        self::assertSame(400, $badRequestResponse->getStatusCode());
+
+        self::assertSame('application/json', $badRequestResponse->getHeaderLine('content-type'));
+
+        $badRequestResponseData = json_decode($badRequestResponse->getBody()->getContents(), true);
+        self::assertIsArray($badRequestResponseData);
+        self::assertArrayHasKey('message', $badRequestResponseData);
+        self::assertSame('User already exists', $badRequestResponseData['message']);
+
+        $user = $this->userRepository->findAll()[0];
+        self::assertInstanceOf(User::class, $user);
+
+        self::assertArrayHasKey('user', $badRequestResponseData);
+        self::assertSame(
+            [
+                'id' => $user->getId(),
+                'user-identifier' => $email,
+            ],
+            $badRequestResponseData['user']
+        );
+    }
+
+    public function testCreateSuccess(): void
+    {
+        self::assertSame(0, $this->userRepository->count([]));
+
+        $email = 'user@example.com';
+        $password = 'password';
+
+        $response = $this->applicationClient->makeAdminCreateUserRequest(
+            $email,
+            $password,
+            $this->getAdminToken()
+        );
+
+        self::assertSame(200, $response->getStatusCode());
+        self::assertSame(1, $this->userRepository->count([]));
+
+        $responseData = json_decode($response->getBody()->getContents(), true);
+        self::assertIsArray($responseData);
+
+        $user = $this->userRepository->findAll()[0];
+        self::assertInstanceOf(User::class, $user);
+
+        self::assertArrayHasKey('user', $responseData);
+        self::assertSame(
+            [
+                'id' => $user->getId(),
+                'user-identifier' => $email,
+            ],
+            $responseData['user']
+        );
+    }
+
+    abstract protected function getAdminToken(): string;
+}
--- tests/Integration/Admin/User/CreateTest.php
+++ tests/Integration/Admin/User/CreateTest.php
@@ -4,61 +4,12 @@ declare(strict_types=1);
 
 namespace App\Tests\Integration\Admin\User;
 
-use App\Tests\Integration\AbstractIntegrationTest;
+use App\Tests\Application\Admin\User\AbstractCreateTest;
+use App\Tests\Integration\Admin\GetAdminTokenTrait;
+use App\Tests\Integration\GetClientAdapterTrait;
 
-class CreateTest extends AbstractIntegrationTest
+class CreateTest extends AbstractCreateTest
 {
-    public function testCreateUnauthorized(): void
-    {
-        $response = $this->application->makeAdminCreateUserRequest(
-            self::TEST_USER_EMAIL,
-            self::TEST_USER_EMAIL,
-            'invalid-token'
-        );
-
-        $this->applicationResponseAsserter->assertAdminUnauthorizedResponse($response);
-    }
-
-    /**
-     * @dataProvider createBadRequestDataProvider
-     */
-    public function testCreateBadRequest(?string $email, ?string $password): void
-    {
-        $adminToken = self::getContainer()->getParameter('primary-admin-token');
-        \assert(is_string($adminToken));
-
-        $response = $this->application->makeAdminCreateUserRequest($email, $password, $adminToken);
-
-        self::assertSame(400, $response->getStatusCode());
-    }
-
-    /**
-     * @return array<mixed>
-     */
-    public function createBadRequestDataProvider(): array
-    {
-        return [
-            'no data' => [
-                'email' => null,
-                'password' => null,
-            ],
-            'email missing' => [
-                'email' => null,
-                'password' => self::TEST_USER_PASSWORD,
-            ],
-            'password missing' => [
-                'email' => self::TEST_USER_EMAIL,
-                'password' => null,
-            ],
-        ];
-    }
-
-    public function testCreateSuccess(): void
-    {
-        $this->removeAllUsers();
-
-        $response = $this->createTestUser();
-
-        $this->applicationResponseAsserter->assertCreateUserSuccessResponse($response, $this->getTestUser());
-    }
+    use GetClientAdapterTrait;
+    use GetAdminTokenTrait;
 }
