e82af86b140b95eb83b0ec0cbff8594c75c670dc
Jon Cram <webignition@gmail.com>
2022-08-18 16:56:22 +0100

    Make ApiKey::label nullable

--- src/Entity/ApiKey.php
+++ src/Entity/ApiKey.php
@@ -19,10 +19,10 @@ class ApiKey
     #[ORM\Column(type: 'string', length: ApiKeyPropertiesInterface::ID_LENGTH, unique: true)]
     private readonly string $id;
 
-    #[ORM\Column(type: 'string', length: 255)]
-    private readonly string $label;
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private readonly ?string $label;
 
-    public function __construct(string $id, string $label, User $owner)
+    public function __construct(string $id, ?string $label, User $owner)
     {
         $this->id = $id;
         $this->label = $label;
