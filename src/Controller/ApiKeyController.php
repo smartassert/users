53f233c12b10f5058855d00ae3f9ea2a9a1aa65d
Jon Cram <webignition@gmail.com>
2022-08-30 14:44:21 +0100

    Provide list of api keys via endpoint

--- src/Controller/ApiKeyController.php
+++ src/Controller/ApiKeyController.php
@@ -5,13 +5,24 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\ApiKeyRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class ApiKeyController
 {
-    public function list(?User $user): Response
+    public function list(ApiKeyRepository $apiKeyRepository, ?User $user): Response
     {
-        return new JsonResponse([]);
+        $serializedApiKeys = [];
+        $apiKeys = $apiKeyRepository->findBy(['owner' => $user]);
+
+        foreach ($apiKeys as $apiKey) {
+            $serializedApiKeys[] = [
+                'label' => $apiKey->label,
+                'key' => $apiKey->id,
+            ];
+        }
+
+        return new JsonResponse($serializedApiKeys);
     }
 }
