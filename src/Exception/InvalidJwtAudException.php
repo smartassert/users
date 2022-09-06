2f2796a25102c46cb310aa4b3ff5482a96ad0872
Jon Cram <webignition@gmail.com>
2022-09-06 16:13:33 +0100

    Consider token aud when authenticating

---
+++ src/Exception/InvalidJwtAudException.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exception;
+
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+
+class InvalidJwtAudException extends AuthenticationException
+{
+    public function __construct(
+        public readonly string $firewallName,
+        public readonly string $jwtAud
+    ) {
+        parent::__construct(sprintf(
+            'Invalid JWT aud claim "%s" for firewall "%s"',
+            $jwtAud,
+            $firewallName
+        ));
+    }
+}
