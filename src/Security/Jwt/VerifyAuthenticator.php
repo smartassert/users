847ee09addeac68052f173fa22148329e4bffc44
Jon Cram <webignition@gmail.com>
2022-09-06 16:47:12 +0100

    Fix CS

--- src/Security/Jwt/VerifyAuthenticator.php
+++ src/Security/Jwt/VerifyAuthenticator.php
@@ -16,7 +16,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
