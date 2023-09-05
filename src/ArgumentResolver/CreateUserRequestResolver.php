b36c19643b9d6f2c0d810ebde2a395df462d8353
Jon Cram <webignition@gmail.com>
2023-09-05 12:14:23 +0100

    Use ValueResolverInterface

--- src/ArgumentResolver/CreateUserRequestResolver.php
+++ src/ArgumentResolver/CreateUserRequestResolver.php
@@ -6,29 +6,26 @@ namespace App\ArgumentResolver;
 
 use App\Request\CreateUserRequest;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
+use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 
-class CreateUserRequestResolver implements ArgumentValueResolverInterface
+class CreateUserRequestResolver implements ValueResolverInterface
 {
-    public function supports(Request $request, ArgumentMetadata $argument): bool
-    {
-        return CreateUserRequest::class === $argument->getType();
-    }
-
     /**
-     * @return \Traversable<CreateUserRequest>
+     * @return CreateUserRequest[]
      */
-    public function resolve(Request $request, ArgumentMetadata $argument): \Traversable
+    public function resolve(Request $request, ArgumentMetadata $argument): iterable
     {
-        if ($this->supports($request, $argument)) {
-            $email = $request->request->get(CreateUserRequest::KEY_EMAIL);
-            $email = is_string($email) ? trim($email) : null;
-
-            $password = $request->request->get(CreateUserRequest::KEY_PASSWORD);
-            $password = is_string($password) ? $password : null;
-
-            yield new CreateUserRequest('' === $email ? null : $email, '' === $password ? null : $password);
+        if (CreateUserRequest::class !== $argument->getType()) {
+            return [];
         }
+
+        $email = $request->request->get(CreateUserRequest::KEY_EMAIL);
+        $email = is_string($email) ? trim($email) : null;
+
+        $password = $request->request->get(CreateUserRequest::KEY_PASSWORD);
+        $password = is_string($password) ? $password : null;
+
+        return [new CreateUserRequest('' === $email ? null : $email, '' === $password ? null : $password)];
     }
 }
