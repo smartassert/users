37a8423597219c753e7a0d618af8e8ec4a283ea8
Jon Cram <webignition@gmail.com>
2023-09-08 15:04:00 +0100

    Replace usage of deprecated Symfony\Component\Security\Core\Security (#391)

--- src/ArgumentResolver/UserEntityResolver.php
+++ src/ArgumentResolver/UserEntityResolver.php
@@ -6,10 +6,10 @@ namespace App\ArgumentResolver;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
-use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 readonly class UserEntityResolver implements ValueResolverInterface
