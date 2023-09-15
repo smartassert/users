788d8b9893c60c742065a9f703756db1d18db499
Jon Cram <webignition@gmail.com>
2023-09-15 10:14:29 +0100

    Use php-cs-fixer 3.26.1 (#402)

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -20,6 +20,7 @@ return (new PhpCsFixer\Config())
         'php_unit_internal_class' => false,
         'php_unit_test_class_requires_covers' => false,
         'declare_strict_types' => true,
+        'single_line_empty_body' => false,
     ])
     ->setFinder($finder)
     ;
