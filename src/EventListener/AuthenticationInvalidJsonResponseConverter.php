93ad9cd54dc8ab4a9f7d05f4bb8844c48ae6ca63
Jon Cram <webignition@gmail.com>
2023-12-12 17:28:57 +0000

    Return 401 if credentials json is missing fields (#472)

--- src/EventListener/AuthenticationInvalidJsonResponseConverter.php
+++ src/EventListener/AuthenticationInvalidJsonResponseConverter.php
@@ -12,6 +12,15 @@ use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 class AuthenticationInvalidJsonResponseConverter implements EventSubscriberInterface
 {
     /**
+     * @var non-empty-string[]
+     */
+    private array $expectedExceptionMessages = [
+        'Invalid JSON.',
+        'The key "username" must be provided.',
+        'The key "password" must be provided.',
+    ];
+
+    /**
      * @return array<class-string, array<mixed>>
      */
     public static function getSubscribedEvents(): array
@@ -26,8 +35,12 @@ class AuthenticationInvalidJsonResponseConverter implements EventSubscriberInter
     public function convertInvalidJsonBadRequestResponseToUnauthorizedResponse(ExceptionEvent $event): void
     {
         $throwable = $event->getThrowable();
-        if ($throwable instanceof BadRequestHttpException) {
-            if ('Invalid JSON.' === $throwable->getMessage()) {
+        if (!$throwable instanceof BadRequestHttpException) {
+            return;
+        }
+
+        foreach ($this->expectedExceptionMessages as $expectedExceptionMessage) {
+            if ($expectedExceptionMessage === $throwable->getMessage()) {
                 $event->setResponse(new UnauthorizedResponse());
                 $event->stopPropagation();
             }
