0c01fed6ebf65e3a48d26a66787943e77d1a0ad5
Jon Cram <webignition@gmail.com>
2024-02-01 12:20:42 +0000

    update revocation of all refresh tokens for user errors

--- src/ArgumentResolver/RevokeRefreshTokenRequestResolver.php
+++ src/ArgumentResolver/RevokeRefreshTokenRequestResolver.php
@@ -5,14 +5,25 @@ declare(strict_types=1);
 namespace App\ArgumentResolver;
 
 use App\Request\RevokeRefreshTokenRequest;
+use SmartAssert\ServiceRequest\Exception\ErrorResponseException;
+use SmartAssert\ServiceRequest\Parameter\Factory;
+use SmartAssert\ServiceRequest\Parameter\Validator\StringParameterValidator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 
-class RevokeRefreshTokenRequestResolver implements ValueResolverInterface
+readonly class RevokeRefreshTokenRequestResolver implements ValueResolverInterface
 {
+    public function __construct(
+        private StringParameterValidator $parameterValidator,
+        private Factory $parameterFactory,
+    ) {
+    }
+
     /**
      * @return RevokeRefreshTokenRequest[]
+     *
+     * @throws ErrorResponseException
      */
     public function resolve(Request $request, ArgumentMetadata $argument): iterable
     {
@@ -20,9 +31,12 @@ class RevokeRefreshTokenRequestResolver implements ValueResolverInterface
             return [];
         }
 
-        $id = $request->request->get(RevokeRefreshTokenRequest::KEY_ID);
-        $id = is_string($id) ? trim($id) : null;
-        $id = '' === $id ? null : $id;
+        $id = $this->parameterValidator->validateNonEmptyString($this->parameterFactory->createStringParameter(
+            RevokeRefreshTokenRequest::KEY_ID,
+            trim($request->request->getString(RevokeRefreshTokenRequest::KEY_ID)),
+            1,
+            null,
+        ));
 
         return [new RevokeRefreshTokenRequest($id)];
     }
--- src/Request/RevokeRefreshTokenRequest.php
+++ src/Request/RevokeRefreshTokenRequest.php
@@ -4,25 +4,15 @@ declare(strict_types=1);
 
 namespace App\Request;
 
-use Symfony\Component\HttpFoundation\Request;
-
 class RevokeRefreshTokenRequest
 {
     public const KEY_ID = 'id';
 
     /**
-     * @param null|non-empty-string $id
+     * @param non-empty-string $id
      */
     public function __construct(
-        public readonly ?string $id,
+        public readonly string $id,
     ) {
-    }
-
-    public static function create(Request $request): RevokeRefreshTokenRequest
-    {
-        $id = $request->request->get(self::KEY_ID);
-        $id = is_string($id) ? trim($id) : null;
-
-        return new RevokeRefreshTokenRequest('' === $id ? null : $id);
     }
 }
