ec5fcbbd8c980328bec9d1187867ba0885f06f0b
Jon Cram <webignition@gmail.com>
2024-03-14 10:10:42 +0000

    Remove redundant test that is failing static analysis checks

--- tests/Functional/Entity/UserTest.php
+++ tests/Functional/Entity/UserTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Tests\Functional\Entity;
 
 use App\Entity\User;
-use App\Repository\UserRepository;
 use App\Tests\Functional\AbstractBaseFunctionalTestCase;
 use App\Tests\Services\TestUserFactory;
 use Doctrine\ORM\EntityManagerInterface;
@@ -14,7 +13,6 @@ use Symfony\Bridge\Doctrine\Security\User\EntityUserProvider;
 class UserTest extends AbstractBaseFunctionalTestCase
 {
     private EntityManagerInterface $entityManager;
-    private UserRepository $userRepository;
     private TestUserFactory $testUserFactory;
 
     protected function setUp(): void
@@ -25,28 +23,11 @@ class UserTest extends AbstractBaseFunctionalTestCase
         \assert($entityManager instanceof EntityManagerInterface);
         $this->entityManager = $entityManager;
 
-        $userRepository = self::getContainer()->get(UserRepository::class);
-        \assert($userRepository instanceof UserRepository);
-        $this->userRepository = $userRepository;
-
         $testUserFactory = self::getContainer()->get(TestUserFactory::class);
         \assert($testUserFactory instanceof TestUserFactory);
         $this->testUserFactory = $testUserFactory;
 
         $this->removeAllUsers();
-    }
-
-    public function testEntityMapping(): void
-    {
-        self::assertCount(0, $this->userRepository->findAll());
-
-        $user = $this->testUserFactory->create();
-        $this->entityManager->detach($user);
-
-        $users = $this->userRepository->findAll();
-
-        self::assertCount(1, $users);
-        self::assertEquals($user, $users[0]);
     }
 
     public function testGetRolesDoesNotThrowUninitializedTypedPropertyError(): void
