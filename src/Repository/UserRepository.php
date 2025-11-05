159a68211a37322773804a1e9f7ef033497101c2
Jon Cram <webignition@gmail.com>
2025-11-05 16:00:45 +0000

    Remove UserRepository implementation of PasswordUpgraderInterface

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -8,9 +8,6 @@ use App\Entity\User;
 use App\Entity\UserPropertiesInterface;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
-use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 
 /**
  * @method null|User find($id, $lockMode = null, $lockVersion = null)
@@ -20,25 +17,11 @@ use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
  *
  * @extends ServiceEntityRepository<User>
  */
-class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
+class UserRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
-    }
-
-    /**
-     * Used to upgrade (rehash) the user's password automatically over time.
-     */
-    public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
-    {
-        if (!$user instanceof User) {
-            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
-        }
-
-        $user->setPassword($newHashedPassword);
-        $this->_em->persist($user);
-        $this->_em->flush();
     }
 
     public function findByUserIdentifier(string $identifier): ?User
